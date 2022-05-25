540d25290a9e69cba9e4d07d0454189a185aa1c4
Jesse Nieboer <jessenieboer@protonmail.com>
2022-05-25 18:35:19 -0500

    exp(): Remove home manager stuff
    I plan on keeping my home manager config in a separate repo

--- flake.nix
+++ flake.nix
@@ -3,13 +3,9 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
-    home-manager = {
-      url = github:nix-community/home-manager;
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, home-manager }:
+  outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -23,7 +19,7 @@
           modules = [
             ./system/common.nix
             ./system/uiu57o6a.nix
-            ./user/jessenieboer.nix
+            ./user/main-laptop-users.nix
           ];
         };
       };
