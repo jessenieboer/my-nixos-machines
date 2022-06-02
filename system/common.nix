9ddd02633f75ae10454b8d012054c6315f4990a4
Jesse Nieboer <jessenieboer@protonmail.com>
2022-06-02 17:37:20 -0500

    feat(): Set up binary cache

--- system/common.nix
+++ system/common.nix
@@ -1,9 +1,19 @@
 { config, pkgs, ... }: {
+
+  nix = {
+    binaryCachePublicKeys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+    binaryCaches =
+      [ "https://cache.nixos.org" "https://nix-community.cachix.org" ];
+  };
+
   environment = {
     systemPackages = with pkgs; [
       git
     ];
   };
-  
+
   users.mutableUsers = false;  
 }
